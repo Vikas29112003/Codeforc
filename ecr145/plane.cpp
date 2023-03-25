3fc7b3e10d4c1d72832bd703c573353e9843e581
Vikas <vikas.singh22@spit.ac.in>
2023-03-25 14:53:46 +0530

    added

---
+++ ecr145/plane.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; t++)
+    {
+long double n,k;
+cin>>n;
+k = sqrt(n);
+if(k == int(k)){
+    cout<<(int(k)-1)<<endl;
+}
+else{
+    cout<<int(k)<<endl;
+}
+    }
+    return 0;
+}
